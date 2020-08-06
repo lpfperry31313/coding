22637f312dc354aca78e9061a0f9baf814bef9e7
lipengfei <li.pengfei@synyi.com>
2020-08-06 17:15:21 +0800

    404

---
+++ tree/111-minimum-depth-of-binary-tree.py
@@ -0,0 +1,28 @@
+"""
+111. 二叉树的最小深度
+https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+假设树上一共N个节点
+时间 O(N)
+    遍历每一个节点
+空间 O(N)
+    最坏情况 退化成链表 O(N)
+"""
+
+
+def solution(self, root):
+    if not root:
+        return 0
+    return self._helper(root, 1)
+
+
+def _helper(t, count):
+    if not t.left and not t.right:
+        return 1
+    elif t.left and not t.right:
+        count += _helper(t.left, count)
+    elif t.right and not t.left:
+        count += _helper(t.right, count)
+    else:
+        count += min(_helper(t.right, count), _helper(t.left, count))
+
+    return count
